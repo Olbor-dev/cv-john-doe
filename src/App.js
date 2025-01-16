bdcda6e5c90595a62cc884de187f4e9f4f5c485b
Olbor-dev <olbor.dev@gmail.com>
2025-01-16 13:56:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />        
+      <h1>Mon site</h1>
+    </div>
+  );
+}
+
+export default App;
