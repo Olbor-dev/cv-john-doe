c9015f2b14447f24a76072c1f3dcefd300b5e9e9
Olbor-dev <olbor.dev@gmail.com>
2025-01-21 12:26:09 +0100

    Page Services

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import screen from '../img/pc.png';
+import dev from '../img/developpement-web.png';
+import seo from '../img/seo.png';
 
 const ServiceCard = () => {
   return (
@@ -12,16 +14,16 @@ const ServiceCard = () => {
             </div>
         </div>
         <div class="card m-2" style={{width: "18rem"}}>
-            <img src={screen} class="rounded mx-auto d-block mt-4" alt="écran" width="50" />
+            <img src={dev} class="rounded mx-auto d-block mt-4" alt="écran" width="50" />
             <div class="card-body">
-                <h5 class="card-title text-center p-3">Développement web</h5>
+                <h5 class="card-title text-uppercase text-center p-3">Développement web</h5>
                 <p class="card-text text-center p-2">Le <strong>développement de site web</strong> repose sur l'utilisation des langages HTML, CSS, JavaScript et PHP.</p>
             </div>
         </div>
         <div class="card m-2" style={{width: "18rem"}}>
-            <img src={screen} class="rounded mx-auto d-block mt-4" alt="écran" width="50" />
+            <img src={seo} class="rounded mx-auto d-block mt-4" alt="écran" width="50" />
             <div class="card-body">
-                <h5 class="card-title text-center p-3">Référencement</h5>
+                <h5 class="card-title text-uppercase text-center p-3">Référencement</h5>
                 <p class="card-text text-center p-2">Le <strong>référencement naturel d'un site</strong>, aussi appelé SEO, consiste à mettre des techniques en oeuvre pour <i>améliorer sa position</i> dans les résultats des moteurs de recherche</p>
             </div>
         </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ServiceCard from '../components/ServiceCard'
+import React from 'react';
+import ServiceCard from '../components/ServiceCard';
+import banner from '../img/banner.jpg';
 
 const Services = () => {
   return (
     <main>
-        <h1>Mon offre de services</h1>
-        <p>Voici les prestations sur lesquelle je peux intervenir.</p>
-        <hr />
-        <ServiceCard />
+        <img src={banner} alt="bannière" class="img-fluid w-100 pb-2" />
+        <div class="container-fluid pb-4">
+            <h1 class="text-center text-uppercase mt-3">Mon offre de services</h1>
+            <p class="text-center">Voici les prestations sur lesquelle je peux intervenir.</p>
+            <hr class="border border-primary border-3 opacity-75 mx-auto mb-4" width="20%" />
+            <ServiceCard />
+        </div>
     </main>
   )
 }
